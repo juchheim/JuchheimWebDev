bac3c38021136799d5000dca1c18ef7446d12e94
Ernest Juchheim <64336436+juchheim@users.noreply.github.com>
2024-06-19 13:21:20 -0500

    Update stripe.js

--- wp-content/themes/juchheim/js/stripe.js
+++ wp-content/themes/juchheim/js/stripe.js
@@ -1,23 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const stripe = Stripe('your_stripe_publishable_key'); // Replace with your Stripe publishable key
+    const stripe = Stripe('pk_test_51PRj4aHrZfxkHCcnhKjEkTIKhaASMGZaE6iDQfHE4MaxcC1xvqfafGBBXEFYOO1AC0In0YwGJbDa4yFeM3DckrGQ00onFkBwh5'); // Replace with your Stripe publishable key
 
     document.querySelectorAll('form').forEach(form => {
         form.addEventListener('submit', async (e) => {
             e.preventDefault();
 
             const formData = new FormData(form);
-            const response = await fetch('/wp-content/themes/juchheim/stripe/stripe-checkout.php', {
-                method: 'POST',
-                body: formData,
-            });
+            try {
+                console.log('Submitting form...');
+                const response = await fetch('/wp-content/themes/juchheim/stripe/stripe-checkout.php', {
+                    method: 'POST',
+                    body: formData,
+                });
 
-            const result = await response.json();
-            const sessionId = result.id;
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
 
-            const { error } = await stripe.redirectToCheckout({ sessionId });
+                const result = await response.json();
+                console.log('Result:', result);
+                const sessionId = result.id;
 
-            if (error) {
-                console.error('Stripe Checkout error:', error);
+                const { error } = await stripe.redirectToCheckout({ sessionId });
+
+                if (error) {
+                    console.error('Stripe Checkout error:', error);
+                }
+            } catch (error) {
+                console.error('Fetch error:', error);
             }
         });
     });
