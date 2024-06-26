82c3a7613449b3e04c04b2f56e7a65f621436062
Ernest Juchheim <64336436+juchheim@users.noreply.github.com>
2024-06-26 10:13:38 -0500

    Update functions.php

--- wp-content/themes/juchheim/functions.php
+++ wp-content/themes/juchheim/functions.php
@@ -237,28 +237,4 @@ function handle_stripe_webhook() {
     http_response_code(200);
 }
 
-
-if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
-}
-
-// Function to test sending an email
-function test_wp_mail_smtp() {
-    $to = 'juchheim@gmail.com'; // Replace with your email address
-    $subject = 'WP Mail SMTP Test';
-    $message = 'This is a test email sent using WP Mail SMTP configured with Elastic Email.';
-    $headers = array('Content-Type: text/html; charset=UTF-8');
-
-    if (wp_mail($to, $subject, $message, $headers)) {
-        echo 'Email sent successfully!';
-    } else {
-        echo 'Failed to send email.';
-    }
-}
-
-// Hook the test function to WordPress init action
-add_action('init', 'test_wp_mail_smtp');
-
-
-
 ?>
