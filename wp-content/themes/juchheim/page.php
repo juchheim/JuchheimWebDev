d02e9b1985ddfd711b664d4ed220cddef2dc916b
Ernest Juchheim <64336436+juchheim@users.noreply.github.com>
2024-07-09 19:11:47 -0500

    added Dream Innovations Inc. to portfolio

--- wp-content/themes/juchheim/page.php
+++ wp-content/themes/juchheim/page.php
@@ -23,46 +23,47 @@ get_header();
         <h5>I'm here to help <em>whenever</em> you need assistance.</h5>
 		<!-- <a href="#"><img class="down-arrow" src="/wp-content/uploads/2024/06/down_arrow.png" /></a> -->
 
+        
+        
+        
         <div id="portfolio"></div>
-        <h1>Portfolio</h1>
-        <?php
-        // Fetch the Pods instance for 'portfolio'
-        $pods = pods('portfolio', array(
-            'limit' => -1 // Ensure we fetch all entries
-        ));
+<h1>Portfolio</h1>
+<?php
+// Fetch the Pods instance for 'portfolio'
+$pods = pods('portfolio', array(
+    'limit' => -1 // Ensure we fetch all entries
+));
 
-        // Check if there are any entries
-        if ($pods->total() > 0) {
-            echo '<div class="portfolio-grid">';
-            
-            // Loop through the Pods entries
-            while ($pods->fetch()) {
-                // Get the image, link, and caption fields
-                $image = $pods->field('image');
-                $link = $pods->field('link');
-                $caption = $pods->field('caption');
+// Check if there are any entries
+if ($pods->total() > 0) {
+    echo '<div class="portfolio-grid">';
+    
+    // Loop through the Pods entries
+    while ($pods->fetch()) {
+        // Get the image and link fields
+        $image = $pods->field('image');
+        $link = $pods->field('link');
 
-                // Output the portfolio item
-                echo '<figure class="portfolio-item">';
-                if ($link) {
-                    echo '<a href="' . esc_url($link) . '" target="_blank">';
-                }
-                if ($image) {
-                    echo '<img src="' . esc_url($image['guid']) . '" alt="' . esc_attr($image['post_title']) . '">';
-                }
-                if ($link) {
-                    echo '</a>';
-                }
-                if ($caption) {
-                    echo '<figcaption>' . esc_html($caption) . '</figcaption>';
-                }
-                echo '</figure>';
-            }
-            
-            echo '</div>';
-        } else {
-            echo '<p>No portfolio items found.</p>';
+        // Output the portfolio item
+        echo '<figure class="portfolio-item">';
+        if ($link) {
+            echo '<a href="' . esc_url($link) . '" target="_blank">';
         }
+        if ($image) {
+            echo '<img src="' . esc_url($image['guid']) . '" alt="' . esc_attr($image['post_title']) . '">';
+        }
+        if ($link) {
+            echo '</a>';
+        }
+        echo '</figure>';
+    }
+    
+    echo '</div>';
+} else {
+    echo '<p>No portfolio items found.</p>';
+}
+
+
 
         if (have_posts()) :
             while (have_posts()) : the_post();
@@ -70,6 +71,8 @@ get_header();
             endwhile;
         endif;
         ?>
+
+
 
 <div class="content-area">
     <main id="main" class="site-main">
