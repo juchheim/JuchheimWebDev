5db967a0f7a21c2698d2bf8443ffa3dbdfc54fd0
Ernest Juchheim <64336436+juchheim@users.noreply.github.com>
2024-07-11 15:41:45 -0500

    Update footer.php

--- wp-content/themes/juchheim/footer.php
+++ wp-content/themes/juchheim/footer.php
@@ -1,6 +1,6 @@
 <footer id="colophon" class="site-footer">
     <div class="site-info">
-        <p>&copy; <?php echo date('Y'); ?> Ernest Juchheim Web Development. All rights reserved.</p>
+        <p>&copy; <?php echo date('Y'); ?> Juchheim Web Development. All rights reserved.</p>
     </div><!-- .site-info -->
 </footer><!-- #colophon -->
 </div><!-- end parallax -->
