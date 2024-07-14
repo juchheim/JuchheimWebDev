5782734b61abdb45920a9403001143ce3540ea71
Ernest Juchheim <64336436+juchheim@users.noreply.github.com>
2024-07-14 12:09:11 -0500

    debugging

--- wp-content/plugins/chat-plugin/server.js
+++ wp-content/plugins/chat-plugin/server.js
@@ -1,28 +1,25 @@
-const express = require('express');
 const https = require('https');
 const fs = require('fs');
 const WebSocket = require('ws');
+const express = require('express');
 
 const app = express();
-
-// Serve static files (index.html, etc.)
 app.use(express.static(__dirname));
 
 // Create HTTPS server
 const server = https.createServer({
   key: fs.readFileSync('/home/1260594.cloudwaysapps.com/whtqgbwgsb/private_html/server.key'),
   cert: fs.readFileSync('/home/1260594.cloudwaysapps.com/whtqgbwgsb/private_html/server.crt')
-}, app);
+});
 
 // Initialize WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (ws, req) => {
+wss.on('connection', (ws) => {
   console.log('New client connected');
 
   ws.on('message', (message) => {
     console.log('Received message:', message);
-    // Here you can add authentication logic and other message handling
     const response = JSON.stringify({ type: 'message', message: `You: ${message}` });
     ws.send(response);
   });
